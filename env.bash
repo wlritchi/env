7b2513d104157e3023f395eeab59e8dabf01a35f
Luc Ritchie <luc.ritchie@gmail.com>
2022-09-25 22:14:36 -0400

    bump recommended xonsh version to 0.13.3

--- env.bash
+++ env.bash
@@ -25,7 +25,7 @@
 # Python from the venv, not Python from pyenv or the system.
 
 
-RECOMMENDED_XONSH_VERSION=xonsh/0.13.0
+RECOMMENDED_XONSH_VERSION=xonsh/0.13.3
 
 
 [ -n "$__ALIAS_STACK" ] && exit # xonsh source-bash or similar
