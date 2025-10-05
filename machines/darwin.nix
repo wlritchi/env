f8d183cb516c7cb97b3cc6548c39a57172a1b5a5
Luc Ritchie <luc.ritchie@gmail.com>
2025-10-04 22:19:16 -0400

    feat(mac): add ollama pin

--- machines/darwin.nix
+++ machines/darwin.nix
@@ -6,6 +6,11 @@ let
       "https://github.com/NixOS/nixpkgs/archive/5b5b46259bef947314345ab3f702c56b7788cab8.tar.gz";
     sha256 = "0sja5xnj2crjiw0p34hyb9f9v6nn8fzl7bhq8y250jgp0f0xll4n";
   }) { system = pkgs.system; };
+  pinOllama = import (builtins.fetchTarball {
+    url =
+      "https://github.com/NixOS/nixpkgs/archive/b8ee30758753f81c0ee6848f1608e6418e36586e.tar.gz";
+    sha256 = "1gah09dhlzarv0v95q60mpqpi1qal5sl0n0zwjf2lv2pwbzs3302";
+  }) { system = pkgs.system; };
 in {
   imports = [ ./common.nix ];
 
@@ -25,5 +30,6 @@ in {
     teleport
     watchexec
     pinDevspace.devspace
+    pinOllama.ollama
   ];
 }
