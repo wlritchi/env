504927557b1db4b61d074a99f0b46ca79b9a81ba
Luc Ritchie <luc.ritchie@gmail.com>
2025-10-19 01:40:06 -0400

    chore(xmonad): no need for k9s wrapper when launched direct

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -112,7 +112,7 @@ myKeys conf@(XConfig {modMask = modMask}) = M.fromList $
   , ((modMask .|. shiftMask .|. controlMask, xK_s     ), mkXPrompt SshPrompt promptCfg noCompletion ssh)
   , ((modMask .|. shiftMask                , xK_m     ), safeSpawn "alacritty" ["-e", "minicom"])
   , ((modMask                              , xK_slash ), safeSpawn "alacritty" ["-e", "elia"])
-  , ((modMask                              , xK_dollar), safeSpawn "alacritty" ["-e", "k9sx"])
+  , ((modMask                              , xK_dollar), safeSpawn "alacritty" ["-e", "k9s"])
   , ((modMask .|. shiftMask .|. controlMask, xK_m     ), mkXPrompt MinicomPrompt promptCfg noCompletion sshMinicom)
   , ((modMask .|. shiftMask                , xK_t     ), mkXPrompt TwitchPrompt promptCfg noCompletion twitch)
   , ((modMask                              , xK_r     ), spawn "slop")
