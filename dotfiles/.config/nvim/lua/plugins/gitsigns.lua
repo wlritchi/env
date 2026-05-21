f42f4a26bd3e4960523dad8528d49fc1e5144993
Luc Ritchie <luc.ritchie@gmail.com>
2026-05-21 04:19:44 -0400

    config(nvim): blame line by default, 200ms delay

--- dotfiles/.config/nvim/lua/plugins/gitsigns.lua
+++ dotfiles/.config/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,10 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 200,
+    },
     signs = {
       add = { text = "█" },
       change = { text = "|" },
